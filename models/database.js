746ee21d2186f5d09511fcddbfde4e7ba6a97e2a
Wesley Silveira <wslc@cin.ufpe.br>
2025-05-16 13:04:25 -0300

    refactor: converting mongodb driver to mongoose

--- models/database.js
+++ models/database.js
@@ -1,29 +1,16 @@
-const {MongoClient} = require('mongodb');
 require('dotenv').config();
+const mongoose = require('mongoose');
 const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri);
-
-let db;
 
 async function conectarMongo() {
     try {
-        await client.connect();
-        db = client.db();
+        await mongoose.connect(uri);
         console.log('✅ Conectado ao MongoDB com sucesso')
     } catch (error) {
         console.log('❌ Erro ao conectar: ', error);
     }
 }
 
-function getDB(){
-    if (!db) {
-        throw new Error('Banco de dados não conectado.');
-    } else {
-        return db;
-    }
-}
-
 module.exports = {
-    conectarMongo,
-    getDB
+    conectarMongo
 }
